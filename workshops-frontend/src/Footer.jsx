179acff40dbb06bc2150230b4a64d5d8f42721bc
JonasBernard <contact@jonas-bernard.dev>
2026-05-20 03:31:46 +0200

    Add thank you modal

--- workshops-frontend/src/Footer.jsx
+++ workshops-frontend/src/Footer.jsx
@@ -3,17 +3,12 @@ import { Modal, ModalBody, ModalHeader } from "flowbite-react";
 import { HiMail } from "react-icons/hi";
 
 export default function Footer() {
-    const [openModal, setOpenModal] = useState(false);
+    const [openImprintModal, setOpenImprintModal] = useState(false);
+    const [openThankYouModal, setOpenThankYouModal] = useState(false);
 
-    return (
-        <div className="text-center flex flex-col items-stretch gap-3 pt-24 p-3 text-sm text-gray-500 dark:text-gray-400 dark:bg-slate-700 ">
-            <div>
-                <span>Gruppen-Tool in der Version {process.env.REACT_APP_VERSION || "(development)"}</span>
-                <span className="mx-2">•</span>
-                <a href="mailto:info@gruppen-tool.de" className="cursor-pointer text-indigo-600 dark:text-indigo-400">E-Mail: info@gruppen-tool.de</a>
-                <span className="mx-2">•</span>
-                <button onClick={() => setOpenModal(true)} className="cursor-pointer text-indigo-600 dark:text-indigo-400">Impressum</button>
-                <Modal show={openModal} onClose={() => setOpenModal(false)} dismissible={true} popup={true} size="2xl">
+    const imprintModal = () => {
+        return (
+            <Modal show={openImprintModal} onClose={() => setOpenImprintModal(false)} dismissible={true} popup={true} size="2xl">
                 <ModalBody className="mt-3 md:mt-8">
                     <ModalHeader><h1 className="text-3xl font-black dark:text-white">Impressum</h1></ModalHeader>
                     <div className="flex flex-col gap-6 p-2">
@@ -46,7 +41,45 @@ export default function Footer() {
                         </div>
                     </div>
                 </ModalBody>
-                </Modal>
+            </Modal>
+        );
+    }
+
+    const thankYouModal = () => {
+        return (
+            <Modal show={openThankYouModal} onClose={() => setOpenThankYouModal(false)} dismissible={true} popup={true} size="2xl">
+                <ModalBody className="mt-3 md:mt-8">
+                    <ModalHeader><h1 className="text-3xl font-black dark:text-white">Dankeschön &lt;3</h1></ModalHeader>
+                    <div className="mt-2 flex flex-col gap-3 p-2">
+                        <p className="text-sm text-gray-500 dark:text-gray-300">
+                            Danke Nils für deine kritischen Nachfragen dazu, ob meine Gewichtung der Wünsche wirklich so sinnvoll ist.
+                        </p>
+                        <p className="text-sm text-gray-500 dark:text-gray-300">
+                            Danke Jona für die ausführlichen und spaßigen Diskussionen über diverse mathematische und philosophische Fragen
+                            von 'total unimodularity' bis zur Frage, wie fair man eigentlich sein sollte.
+                        </p>
+                        <p className="text-sm text-gray-500 dark:text-gray-300">
+                            Danke an alle, die das Gruppen-Tool nutzen und mir damit die Motivation geben, es weiter zu verbessern,
+                            vor allem diejenigen, die mir so zahlreich konstruktives Feedback geben und Fehler finden.
+                        </p>
+                    </div>
+                </ModalBody>
+            </Modal>
+        );
+    }
+
+    return (
+        <div className="text-center flex flex-col items-stretch gap-3 pt-24 p-3 text-sm text-gray-500 dark:text-gray-400 dark:bg-slate-700 ">
+            <div>
+                <span>Gruppen-Tool in der Version {process.env.REACT_APP_VERSION || "(development)"}</span>
+                <span className="mx-2">•</span>
+                <a href="mailto:info@gruppen-tool.de" className="cursor-pointer text-indigo-600 dark:text-indigo-400">E-Mail: info@gruppen-tool.de</a>
+                <span className="mx-2">•</span>
+                <button onClick={() => setOpenImprintModal(true)} className="cursor-pointer text-indigo-600 dark:text-indigo-400">Impressum</button>
+                {imprintModal()}
+                <span className="mx-2">•</span>
+                <button onClick={() => setOpenThankYouModal(true)} className="cursor-pointer text-indigo-600 dark:text-indigo-400">Dankeschön</button>
+                {thankYouModal()}
             </div>
         </div>
     );
